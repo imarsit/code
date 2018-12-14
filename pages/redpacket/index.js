989504f5f404868542aa61fa22d0d10305b7be2a
sixone <931039665@qq.com>
2018-12-14 15:00:10 +0800

    删除多余数据,添加借口

--- pages/redpacket/index.js
+++ pages/redpacket/index.js
@@ -1,15 +1,27 @@
 Page({
 	data: {
 		open: true,
-		after: false,
-		price:'88'
+		price: '88.88'
 	},
 	open() {
-		this.setData({
-			open: false,
-			after: true
+		this.setData({ open: false })
+
+	},
+	load() {
+		wx.showLoading({ title: '加载中' })
+		return ContentStore.get(name, _id).then(doc => {
+			let { price } = doc
+
+
+			price = (parseFloat(price) || 0).toFixed(2)
+
+			this.setData({ price })
+		}).catch(err => {
+			let { message } = err
+			wx.showToast({ title: message, icon: 'none' })
+		}).then(() => {
+			wx.hideLoading()
 		})
-		
 	}
 
 })
