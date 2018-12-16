9be033b8b561bfeaf9e4443110a40b5243a332fb
sixone <931039665@qq.com>
2018-12-16 19:48:54 +0800

    优惠券修改

--- pages/coupon/index.js
+++ pages/coupon/index.js
@@ -1,25 +1,44 @@
 Page({
 	data: {
+		gtype: false,
 		items: [
 			{
-			img: '../../images/coupon/demo.png',
-			title: '5元全品类券',
-			price: '5',
-			condition:'满120可用'
+				img: '../../images/coupon/demo.png',
+				title: '5元全品类券',
+				price: '5',
+				condition: '满120可用',
+				ctype: '立即领取',
+				gettype: ''
 			},
 			{
 				img: '../../images/coupon/demo.png',
 				title: '5元全品类券',
 				price: '5',
-				condition: '满120可用'
+				condition: '满120可用',
+				ctype: '立即领取'
+
 			},
 			{
 				img: '../../images/coupon/demo.png',
 				title: '5元全品类券',
 				price: '5',
-				condition: '满120可用'
+				condition: '满120可用',
+				ctype: '立即领取'
 			}
 
 		]
+	},
+	get(e) {
+		let id = e.currentTarget.dataset.id;
+		console.log(id)
+		let showtype = "items[" + id + "].ctype";
+		let gettype = "items[" + id + "].gettype";
+
+		console.log(gettype)
+		this.setData({
+			gtype: true,
+			[showtype]: '已领取',
+			[gettype]: '../../images/coupon/geted.png'
+		})
 	}
 })
