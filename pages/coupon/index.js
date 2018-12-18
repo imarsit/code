a11491749968cecbc7122d348752a370b7675861
sixone <931039665@qq.com>
2018-12-18 22:28:52 +0800

    优惠券修改

--- pages/coupon/index.js
+++ pages/coupon/index.js
@@ -1,54 +1,64 @@
 Page({
 	data: {
-		gtype: false,
+
 		items: [
 			{
+				id: '1',
 				img: '../../images/coupon/demo.png',
 				title: '5元全品类券',
 				price: '5',
 				condition: '满120可用',
-				ctype: '立即领取',
-				bg: 'contdraw'
+				type: false
 			},
 			{
+				id: '2',
 				img: '../../images/coupon/demo.png',
 				title: '15元全品类券',
 				price: '15',
 				condition: '满120可用',
-				ctype: '立即领取',
-				bg: 'contdraw'
+				type: false
 
 			},
 			{
+				id: '3',
 				img: '../../images/coupon/demo.png',
 				title: '50元全品类券',
 				price: '50',
 				condition: '满100可用',
-				ctype: '立即领取',
-				bg: 'contdraw'
+				type: false
 			}
 
-		]
+		],
+		coupon: []
+
+
 	},
 	get(e) {
-		let id = e.currentTarget.dataset.id;
-		console.log(id)
+		// 1.获取唯一标记
+		// 2.找到要循环的变量
+		// 3.循环变量
+		// 4.比较唯一标记，查找指定项
+		// 5.指定项赋值
+		// 6.判断是否找到指定项
+		// 7.如果有
+		// 7.1 赋值
+		// 7.2 反向赋值
+		// 8.如果没有
 
-		let showtype = "items[" + id + "].ctype";
-		console.log(showtype)
+		let index = e.currentTarget.dataset.index
+		let items = this.data.items
+		let results = items[index]
 
-		let gettype = "items[" + id + "].gettype";
-		console.log(gettype)
+		if (results) {
 
-		let bg = "items[" + id + "].bg";
-		console.log(bg)
+			results.type = !results.type ? true : results.type
 
-		this.setData({
-			id: id,
-			gtype: true,
-			[showtype]: '已领取',
-			[bg]:'after',
-			[gettype]: '../../images/coupon/geted.png'
-		})
+			items[index] = results
+
+			this.setData({items})
+		}
+
+
+
 	}
 })
